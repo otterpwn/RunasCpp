075633400715e684489b53b8ca6a2132c258a74c
otter <otter@root.com>
2024-06-25 18:31:21 +0200

    successfully create process as different user

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <Windows.h>
+#include <string>
 
 #include "parser.h"
 #include "util.h"
 
 int main(int argc, char* argv[]) {
     ArgumentParser parser;
+
+    STARTUPINFOW si;
+    PROCESS_INFORMATION pi;
+
+    RtlSecureZeroMemory(&si, sizeof(STARTUPINFO));
+    RtlSecureZeroMemory(&pi, sizeof(PROCESS_INFORMATION));
 
     if (!parser.ParseArguments(argc, argv)) return 1;
 
@@ -18,9 +26,9 @@ int main(int argc, char* argv[]) {
     std::string domain;
     std::vector<std::string> processArgs = {};
 
+    // if domain is empty, use hostname
     if (!parser.GetDomain().empty()) {
         domain = parser.GetDomain();
-        std::cout << "domain supplied: " << domain << std::endl;
     } else {
         domain = getLocalHostname();
         if (domain.empty()) {
@@ -30,6 +38,24 @@ int main(int argc, char* argv[]) {
     }
 
     processArgs = parser.GetArgs();
+    LPWSTR parsedProcessArgs = joinArguments(processArgs);
+
+    std::wstring wUsername = toWideString(username);
+    std::wstring wPassword= toWideString(password);
+    std::wstring wProcessPath= toWideString(processPath);
+    std::wstring wDomain = toWideString(domain);
+
+
+    if (!CreateProcessWithLogonW(wUsername.c_str(), wDomain.c_str(), wPassword.c_str(), LOGON_WITH_PROFILE, wProcessPath.c_str(), parsedProcessArgs, 0, NULL, NULL, &si, &pi)) {
+        printf("[!] Error while creating process as %S\\%S: %lu\n", wDomain.c_str(), wUsername.c_str(), GetLastError());
+        return 1;
+    }
+
+    if (pi.dwProcessId > 0)
+        printf("[~] Spawned process with PID %lu as %S\\%S\n", pi.dwProcessId, wDomain.c_str(), wUsername.c_str());
+
+    CloseHandle(pi.hThread);
+    CloseHandle(pi.hProcess);
 
     return 0;
 }
